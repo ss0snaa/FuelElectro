fb338ad2e8f99e7e1e6f2e9a439e57773380b052
ss0snaa <victorsosnovskiy@gmail.com>
2025-02-11 19:46:44 +0300

    Refactor Record data class
    - Add `typeOfWork` property with default value "D".
    - Change `azsPetrol`, `aszLpg`, `prevWSPetrol`, `prevWSLpg` properties to type `Double` and set default value to `0.0`.
    - Add default value for `dateRecord`.

--- app/src/main/java/com/ihff/fuelelectro/data/model/Record.kt
+++ app/src/main/java/com/ihff/fuelelectro/data/model/Record.kt
@@ -6,20 +6,24 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "records")
 data class Record(
     @PrimaryKey(autoGenerate = true) val id: Long = 0,
+
     val dateRecord: Long = System.currentTimeMillis(),
+
+    val typeOfWork: String = "D",
+
     val nowWSD1: Int = 0,
     val nowWSD2: Int = 0,
     val nowWSD3: Int = 0,
     val nowWSOdometer: Int = 0,
 
-    val azsPetrol: Int = 0,
-    val aszLpg: Int = 0,
+    val azsPetrol: Double = 0.0,
+    val azsLpg: Double = 0.0,
 
     val cbxAzs: Boolean = false,
     val cbx2Percent: Boolean = false,
     val cbxCoefficient: Boolean = false,
 
     val prevWSOdometer: Int = 0,
-    val prevWSPetrol: Int = 0,
-    val prevWSLpg: Int = 0
+    val prevWSPetrol: Double = 0.0,
+    val prevWSLpg: Double = 0.0
 )
