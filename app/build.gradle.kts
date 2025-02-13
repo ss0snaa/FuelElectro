59a4d09b107a09f472e9ca0d47d7c728c9a943c8
ss0snaa <victorsosnovskiy@gmail.com>
2025-02-13 12:16:56 +0300

    Update app version to 0.3.0
    - Update `versionName` from "0.2.0" to "0.3.0" in `build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = 1
-        versionName = "0.2.0"
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         versionNameSuffix = "-alpha"
